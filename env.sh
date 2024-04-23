0bdcd9f1657e38b624982778c1432fe1407c58a8
fabrizio <fdellagiacoma@efs.at>
2024-04-23 09:31:24 +0200

    removed files

--- env.sh
+++ env.sh
@@ -1,16 +1,8 @@
-# export BUCKET_INPUT_BILLING="vwag-dpp-billing-detailed-billing-reports"
-# export BUCKET_INPUT_MANUAL="vw-lambda-reporting-manual-input"
-# export BUCKET_OUTPUT="test-dev-automation"
-# export BUCKET_MERCUR_OUTPUT="consumer-bucket-471685057907"
-# export BUCKET_MERCUR_OUTPUT_TEST="usage-testing"
+
 export BUCKET_INPUT='vw-lambda-reporting-output'
-# export BUCKET_INPUT_METERING="consumer-bucket-471685057907"
-# export BUCKET_INPUT_METERING_LOCAL="vw-lambda-reporting-output"
-# export SECRETS_MANAGER="arn:aws:secretsmanager:eu-west-1:471685057907:secret:dpp-dco-CAST-VT5LkZ"
 export local="running local"
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=1
 export branch=$(git symbolic-ref --short HEAD)
-# export BUCKET_CROSS_ACCOUNT="dpp-dco-metering-cross-account"
 
 if [ $branch = DEV ]
 then
