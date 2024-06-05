cfa2ad129d47ccd0acc568069bbe748eb9bf35c7
fabrizio <fdellagiacoma@efs.at>
2024-06-05 11:34:03 +0200

    change bandit target

--- deployment/scripts/code-scan.sh
+++ deployment/scripts/code-scan.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 pip install bandit
-bandit cost-reporting/  -r -c deployment/bandit.yaml  --severity-level high
+bandit app/  -r -c deployment/bandit.yaml  --severity-level high
 
-bandit metering/  -r -c deployment/bandit.yaml  --severity-level high
-
-bandit cast_package/  -r -c deployment/bandit.yaml  --severity-level high
+bandit trigger/  -r -c deployment/bandit.yaml  --severity-level high
